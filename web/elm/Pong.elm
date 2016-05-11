ac6e4cca26ee3996e3ad204eae83a2975ecc6e60
Carsten Rösnick-Neugebauer <carsten.roesnick-neugebauer@tngtech.com>
2016-05-11 16:42:49 +0200

    Add very basic Elm Pong

--- web/elm/Pong.elm
+++ web/elm/Pong.elm
@@ -1,7 +1,70 @@
 module Pong where
 
-import Html
+import Color exposing (green)
+import Graphics.Collage exposing (Form, collage, filled, move, rect)
+import Graphics.Element exposing (Element, layers)
+import Keyboard
+import Signal
+import Text as T
+import Time exposing (Time, fps, inSeconds)
+import Window
+-- import Html
 
-main =
-  Html.text "Let's play Pong!"
+main : Signal Element
+main = Signal.map view gameSignal
+
+
+-- MODEL
+
+type alias State =
+  { x: Float
+  }
+
+initialState : State
+initialState =
+  { x = 0
+  }
+
+
+-- UPDATE
+
+type Event = BoxDx Int
+
+update : Event -> State -> State
+update event state =
+  case event of
+    BoxDx dx ->
+      { state | x = state.x + 4 * toFloat dx }
+
+
+-- VIEW
+
+view : State -> Element
+view state =
+  layers [
+    collage 440 440 [
+      box state.x
+    ]
+  ]
+
+box : Float -> Form
+box x =
+  rect (toFloat 100) (toFloat 20)
+  |> filled green
+  |> move (x, -210)
+
+
+-- SIGNALS
+
+timeDelta : Signal Time
+timeDelta =
+  Signal.map inSeconds (fps 35)
+
+keyboardSignal : Signal Event
+keyboardSignal =
+  Signal.sampleOn timeDelta <|
+    Signal.map BoxDx (Signal.map .x Keyboard.arrows)
+
+gameSignal : Signal State
+gameSignal = Signal.foldp update initialState keyboardSignal
 
